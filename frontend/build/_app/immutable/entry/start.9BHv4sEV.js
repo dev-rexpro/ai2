00a8810daab521beb745d45c5e044cdd925d2985
fdrah <fdrah@rexcorp1.com>
2026-06-28 12:32:56 +0700

    build-1.3

--- frontend/build/_app/immutable/entry/start.9BHv4sEV.js
+++
@@ -1,5 +0,0 @@
-import { l, a } from "../chunks/BLfIdBii.js";
-export {
-  l as load_css,
-  a as start
-};
---
+++ frontend/build/_app/immutable/nodes/18.U8s4R7I5.js
@@ -0,0 +1,9 @@
+import "../chunks/DKem_M_z.js";
+import "../chunks/DwLkIEu4.js";
+import { S as Settings } from "../chunks/aGsvl9sV.js";
+function _page($$anchor) {
+  Settings($$anchor, {});
+}
+export {
+  _page as component
+};
